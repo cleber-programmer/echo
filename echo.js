0edd1a881e79542ccb092cc589e367631bca4fb9
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2015-11-05 17:27:13 +0000

    [echo.AtomicES] Monitora se o elemento esta visivel ou nao visivel

---
+++ echo.js
@@ -0,0 +1,47 @@
+this.Atomic.module('$echo', [
+
+  '$cond',
+  '$forEach',
+  '$push',
+  '$t'
+
+], function ($cond, $forEach, $push, $t) {
+
+  var handlers = [];
+
+  function echo() {
+    $forEach(handlers, scan());
+  }
+
+  function inView(element) {
+    (element.inView || stub)();
+  }
+
+  function outView(element) {
+    (element.outView || stub)();
+  }
+
+  function scan() {
+    return $cond([
+      [scrolledIntoView, inView],
+      [$t, outView]
+    ]);
+  }
+
+  function scrolledIntoView(element) {
+    return compare(element.getBoundingClientRect());
+  }
+
+  function compare(rect) {
+    return rect.top <= document.documentElement.clientHeight && rect.bottom <= 0;
+  }
+
+  function stub() {
+
+  }
+
+  window.addEventListener('scroll', echo, false);
+
+  return Object.defineProperty(echo, 'push', { value: $push(handlers) });
+
+});
